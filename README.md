29ba470b3a7ba27d745c4b738277429755ed15f4
9264YD <yolanda9264d@hotmail.com>
2024-07-01 22:57:27 +0800

    Add files

---
+++ README.md
@@ -0,0 +1,87 @@
+# AI Chart Agent
+
+## Description
+
+This chart agent app helps you generate a simple chart based on the topic of your choice and online data.
+
+![Screenshot](./src/chat.png)
+
+## Getting Started
+
+To get started on your local machine, follow the steps below:
+
+### Setting Up Your OpenAI API Key
+
+1. Open a terminal and navigate to the `server` directory:
+
+   ```bash
+   cd server
+   ```
+
+2. Create a new file named `.env` to store your environment variables:
+
+   ```bash
+   touch .env
+   ```
+
+3. Open the `.env` file in a text editor of your choice, and add the following line, replacing `<your_api_key>` with your actual OpenAI API key:
+
+   ```dotenv
+   # .env file
+   OPENAI_API_KEY=<your_api_key>
+   ```
+
+4. Save the `.env` file and update your terminal session to include the new environment variables:
+
+   ```bash
+   source .env
+   ```
+
+### Backend (FastAPI)
+
+1. Navigate to the project directory:
+
+   ```bash
+   cd server
+   ```
+
+2. Create a virtual environment and activate it (MacOS):
+
+   ```bash
+   python -m venv venv
+   source venv/bin/activate
+   ```
+
+3. Install the required dependencies using `pip`:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+4. Start the FastAPI server using `uvicorn`:
+
+   ```bash
+   uvicorn app:app --host 0.0.0.0 --port 5000 --reload
+   ```
+
+### Frontend (React)
+
+1. Navigate to the project directory:
+
+   ```bash
+   cd client
+   ```
+
+2. Install the required dependencies using `npm`:
+
+   ```bash
+   npm install
+   ```
+
+3. Run the app in development mode:
+
+   ```bash
+   npm start
+   ```
+
+4. Open [http://localhost:3000](http://localhost:3000) to view the app in your browser.
---
+++ server/chart_function.py
@@ -0,0 +1,53 @@
+rom dotenv import load_dotenv, find_dotenv
+from groq import Groq
+import json
+from openai import OpenAI
+import os
+
+load_dotenv(find_dotenv())
+
+client = OpenAI(
+    api_key=os.getenv('OPENAI_API_KEY'),
+)
+llm_model =  'gpt-4o'
+
+def generate_chart(query, query_history):
+    message_text = [
+        {
+            "role": "system",
+            "content": (
+                '''
+                Your task is to generate interactive and visually appealing Chart.js code based on a provided user query: '{query}'.
+                You are expected to analyse the query and determine the appropriate type of chart (e.g., bar, line, pie) that best represents the data. If 'query' does not mention information about a chart do not create a chart and leave the 'chartjs_code' value empty and respond to the user query in 'response' value.
+                Always use the real world data points and label names. Label names need to be specific and cannot be something like 'Label 1' or 'Movie 1'. Be sure to also use professional color codes.
+                For the 'chartjs_code' value, Add Chart.js settings code in json format (EXAMPLE: { type: 'pie', data: { labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'], datasets: [ { data: [30, 20, 15, 10, 25], backgroundColor: [ 'rgba(255, 99, 132, 0.7)', 'rgba(54, 162, 235, 0.7)', 'rgba(255, 206, 86, 0.7)', 'rgba(75, 192, 192, 0.7)', 'rgba(153, 102, 255, 0.7)', ], }, ], }, options: { responsive: true, maintainAspectRatio: false, aspectRatio: 1, plugins: { legend: { display: true, position: 'top', }, }, }, }), nothing else.
+                The example JSON formatted output is the following:
+                {
+                    "response": "",
+                    "chartjs_code": ""
+                }
+                '''
+            )
+        },
+        {
+            "role": "user",
+            "content": query
+        },
+    ]
+
+    completion = client.chat.completions.create(
+        model=llm_model,
+        messages=message_text,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+
+    print('Token usage: ' + str(completion.usage))
+
+    response_content = json.loads(completion.choices[0].message.content)
+    
+    return response_content
